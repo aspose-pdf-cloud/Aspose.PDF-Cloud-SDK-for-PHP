39a85476d7cc1a88d2b9bf28e338a98d66b9a05b
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-03-14 02:41:41 +0200

    Update replaceFoundLink.php

--- UsesCases/Links/replace/replaceFoundLink.php
+++ UsesCases/Links/replace/replaceFoundLink.php
@@ -2,7 +2,9 @@
 
 require __DIR__.'\..\..\vendor\autoload.php';
 
+use Aspose\PDF\Configuration;
 use Aspose\PDF\Api\PdfApi;
+use Aspose\PDF\Model\LinkAnnotation;
 
 $configParams = [
     'LOCAL_FOLDER' => 'C:\\Samples\\',
@@ -13,70 +15,66 @@ $configParams = [
     'NEW_LINK_ACTION' => 'https://reference.aspose.cloud/pdf/#/',               // Your new link action for link ID...
 ];
 
-$credentials = json_decode(file_get_contents('./Credentials/credentials.json'), true);
-$pdfApi = new PdfApi($credentials['id'], $credentials['key']);
-
 class PdfLinks {
-    private $configParams;
     private $pdfApi;
+    private $configParams;
 
-    public function __construct($configParams, $pdfApi) {
-        $this->configParams = $configParams;
-        $this->pdfApi = $pdfApi;
+    private function _create_rest_api() {
+        $credentials = json_decode(file_get_contents("./Credentials/credentials.json"), true);
+
+        $configAuth = new Configuration();
+        $configAuth->setAppKey($credentials['key']);
+        $configAuth->setAppSid($credentials['id']);
+
+        $this->pdfApi = new PdfApi(null, $configAuth);
+     }
+
+    public function __construct($config) {
+        $this->configParams = $config;
+        $this->_create_rest_api();
     }
 
-    public function upload_document() {
+    public function uploadDocument() {
         $pdfFilePath = $this->configParams['LOCAL_FOLDER'] . $this->configParams['PDF_DOCUMENT_NAME'];
         $pdfFileData = file_get_contents($pdfFilePath);
         $this->pdfApi->uploadFile($this->configParams['PDF_DOCUMENT_NAME'], $pdfFileData);
     }
 
-    public function download_result() {
+    public function downloadResult() {
         $changedPdfData = $this->pdfApi->downloadFile($this->configParams['PDF_DOCUMENT_NAME']);
         $filePath = $this->configParams['LOCAL_FOLDER'] . $this->configParams['LOCAL_RESULT_DOCUMENT_NAME'];
         file_put_contents($filePath, $changedPdfData);
         echo "Downloaded: " . $filePath . "\n";
     }
 
-    public function show_links($links, $prefix) {
-        if (is_array($links) && count($links) > 0)
-        {
-            foreach ($links as $link) {
-                echo $prefix . " => '" . $link->id . "', '" . $link->action;
-            }
-        }
-    }
-        
-    public function get_link_by_id() {
+    public function getPageLinkById(): ?LinkAnnotation {
         $result_link = $this->pdfApi->getPageLinkAnnotation($this->configParams['PDF_DOCUMENT_NAME'], $this->configParams['PAGE_NUMBER'], $this->configParams['LINK_FIND_ID']);
 
         if ($result_link->getCode() == 200) {
-            $this->show_links([ $result_link->link ], 'found');
-            return $result_link->link;
+            echo "Found link:";
+            var_dump($result_link->getLink());
+            return $result_link->getLink();
         }
         else{
             echo "Unexpected error : can't get link!!!";
-            return null;
+            return NULL;
         }
     }
 
-    public function replace_link () {
-        $link = $this->get_link_by_id($this->configParams['LINK_REMOVE_ID']);
+    public function replaceLinkById() {
+        $link = $this->getPageLinkById($this->configParams['LINK_REMOVE_ID']);
 
         if ($link)
         {
-            $link->action = $this->configParams['LNEW_LINK_ACTION'];
+            $link->setAction($this->configParams['LNEW_LINK_ACTION']);
             
             $update_response = $this->pdfApi->putLinkAnnotation($this->configParams['PDF_DOCUMENT_NAME'], $this->configParams['LINK_REMOVE_ID'], $link);
 
             if ($update_response->getCode() == 200) {
-                $this->show_links( [ $update_response->link ], "drop");
-                return $update_response->link;
+                echo "Link '" . $this->configParams['LINK_REMOVE_ID'] . "' was replaced !";
             }
-            else{
-                echo "Unexpected error : can't append link!!!";
-                return null;
-            }
+            else
+                echo "Unexpected error : can't replace link!!!";
         }
         else {
             echo "Unexpected error : can't get link!!!";
@@ -88,9 +86,9 @@ class PdfLinks {
 $pdfLinks = new PdfLinks($configParams, $pdfApi);
 
 try {
-    $pdfLinks->upload_document();
-    $pdfLinks->replace_link();
-    $pdfLinks->download_result();
+    $pdfLinks->uploadDocument();
+    $pdfLinks->replaceLinkById();
+    $pdfLinks->downloadResult();
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage() . "\n";
 }
